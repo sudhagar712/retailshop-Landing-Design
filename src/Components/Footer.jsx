30e87f8ecccecf41ae04281e62a710a924d578b4
sudhagar712 <sudhagarmsr712@gmail.com>
2024-11-22 17:26:49 +0530

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,13 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-500 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p>
-          © 2024 ASPV International Production Pvt Ltd. All rights reserved.
+   <footer class="container p-5 bg-gray-200 shadow">
+        <div class="flex flex-col md:flex-row items-center gap-8">
+          <div class="w-full md:w-1/2">
+            <p class="cardtitle">News Letter</p>
+            <p>
+              Sign up for our free Video Course and Urban garden inspiration
+            </p>
+          </div>
+
+          <div class="w-full md:w-1/2">
+            <div class="flex flex-col md:flex-row gap-2">
+              <input
+                class="w-full px-5 py-3 text-bodyColor outline-none"
+                type="email"
+                placeholder="Enter your email Address...."
+              />
+              <button className=" bg-yellow-500 p-4">Subscribe</button>
+            </div>
+          </div>
+        </div>
+        <p class="text-center mt-10 opacity-30">
+          Copyrights &copy; 2024 sudhagarDEV. All rights reserved.
         </p>
-      </div>
-    </footer>
+      </footer>
   );
 };
 
