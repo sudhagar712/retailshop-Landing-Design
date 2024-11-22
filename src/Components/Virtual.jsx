e84dbcb53d55027539adeccf6dc519434b7e958e
sudhagar712 <sudhagarmsr712@gmail.com>
2024-11-22 20:24:56 +0530

    commit

--- src/Components/Virtual.jsx
+++ src/Components/Virtual.jsx
@@ -6,16 +6,16 @@ import shade from "../assests/images/shade.png";
 
 const Virtual = () => {
   return (
-    <div className="container bg-gradient-to-r from-yellow-500 via-gray-300 to-white  shadow-xl">
-      <div className="grid md:grid-cols-2 gap-10 mt-5">
+    <div className="container  bg-gradient-to-r from-yellow-500 via-gray-300 to-white  shadow-xl">
+      <div className="grid md:grid-cols-2 gap-2 mt-5">
         <div className="md:grid-cols-6  p-10">
-          <h1 className=" font-extrabold text-4xl ">VIRTUAL TRY-ON</h1>
-          <h3 className="text-2xl  mt-9">NEVER BUY THE WRONG SHADE AGAIN</h3>
-          <h1 className="font-extrabold text-3xl mt-10 mb-10">Try Now!</h1>
+          <h1 className=" font-extrabold sm:text-5xl text-3xl ">VIRTUAL TRY-ON</h1>
+          <h3 className="text-sm  mt-9">NEVER BUY THE WRONG SHADE AGAIN</h3>
+          <h1 className="font-extrabold text-xl mt-10  ">Try Now!</h1>
 
-          <img src={shade} />
+          <img src={shade} className=" " />
         </div>
-        <div className="md:grid-cols-6  mt-5">
+        <div className="md:grid-cols-6 ">
           <ReactCompareImage leftImage={beforeimage} rightImage={afterimage} />
         </div>
       </div>
